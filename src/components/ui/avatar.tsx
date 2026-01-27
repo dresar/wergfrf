aad41fad9b3d942ebec4e16c8ed7a1a831202b05
EKA SYARIF MAULANA <eka.ckp16799@gmail.com>
2026-01-27 13:59:07 +0700

    Update avatar.tsx

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -16,12 +16,12 @@ const Avatar = React.forwardRef<
 Avatar.displayName = AvatarPrimitive.Root.displayName;
 
 const AvatarImage = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Image>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+  HTMLImageElement,
+  React.ImgHTMLAttributes<HTMLImageElement>
 >(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image ref={ref} className={cn("aspect-square h-full w-full", className)} {...props} />
+  <img ref={ref} className={cn("aspect-square h-full w-full", className)} {...props} />
 ));
-AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+AvatarImage.displayName = "AvatarImage";
 
 const AvatarFallback = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Fallback>,
